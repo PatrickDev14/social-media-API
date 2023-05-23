1787b130562911e7480d59a294191eccb7c7ef4d
Patrick Lauher <plauher14@gmail.com>
2023-05-23 17:35:41 -0500

    add to User based files
    updates to User, Controller, Repository, ServiceImpl files. User.java
    currently has a reference to Credentials that won't build.

--- src/main/java/com/cooksys/twitterAPI/repositories/UserRepository.java
+++ src/main/java/com/cooksys/twitterAPI/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package com.cooksys.twitterAPI.repositories;
 
-public interface UserRepository {
+import com.cooksys.twitterAPI.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
