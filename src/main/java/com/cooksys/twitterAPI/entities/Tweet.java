3a21e596b4e583385d521004f63a96d6e9a6e2f9
Patrick Lauher <plauher14@gmail.com>
2023-05-23 18:34:38 -0500

    add to Tweet based files
    updates to Tweet, Controller, Repository, ServiceImpl files.

--- src/main/java/com/cooksys/twitterAPI/entities/Tweet.java
+++ src/main/java/com/cooksys/twitterAPI/entities/Tweet.java
@@ -1,4 +1,38 @@
 package com.cooksys.twitterAPI.entities;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor
+@Data
 public class Tweet {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    private Timestamp posted;
+
+    private boolean deleted;
+
+    @Nullable
+    private String content;
+
+    @Nullable
+    @ManyToOne
+    @JoinColumn
+    private Tweet inReplyTo;
+
+    @Nullable
+    @ManyToOne
+    private Tweet repostOf;
 }
