cfc59fd598637b842312a91961af326918beeda8
Noahb26 <noahbrown2663@gmail.com>
2023-05-23 10:31:26 -0700

    pushing file structure & dependencies

---
+++ src/main/java/com/cooksys/twitterAPI/mappers/UserMapper.java
@@ -0,0 +1,7 @@
+package com.cooksys.twitterAPI.mappers;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public class UserMapper {
+}
---
+++ src/test/java/com/cooksys/twitterAPI/TwitterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.twitterAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
